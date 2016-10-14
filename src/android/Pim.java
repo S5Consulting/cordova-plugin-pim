2dc813ccac7030ba42439a642c07299f8198ecd9
Svein Gunnar Larsen <Svein Gunnar Larsen>
2016-10-14 11:39:00 +0200

    Dev

--- src/android/Pim.java
+++ src/android/Pim.java
@@ -18,16 +18,25 @@ public class Pim extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
     	if (action.equals("open")) {
 
-
-		try {
-			myPayPoint.open ("/DEV/BT_PAN", "v1.0.0");
-			callbackContext.error("OK");
-			return true;
-		} catch (IllegalAppVersionException | ComAlreadyInitialisedException | ComNotInitialisedException | IllegalIpAddressException e) {
-			callbackContext.error("Error: " + e.printStackTrace());
-			return false;
+			try {
+				myPayPoint.open ("/DEV/BT_PAN", "v1.0.0");
+				callbackContext.success("OK");
+				return true;
+			} catch (IllegalAppVersionException e) {
+				callbackContext.error("Error: " + e.printStackTrace());
+				return false;
+			} catch (ComAlreadyInitialisedException e) {
+				callbackContext.error("Error: " + e.printStackTrace());
+				return false;
+			} catch (ComNotInitialisedException e) {
+				callbackContext.error("Error: " + e.printStackTrace());
+				return false;
+			} catch (IllegalIpAddressException e) {
+				callbackContext.error("Error: " + e.printStackTrace());
+				return false;
+			}
 		}
-    	return false;
+		return false;
     }
 }
 
