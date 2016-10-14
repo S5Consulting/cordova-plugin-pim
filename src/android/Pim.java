8b8ec9c5980490e4d1dfbed4e44c318212e4efe7
Svein Gunnar Larsen <Svein Gunnar Larsen>
2016-10-14 11:12:32 +0200

    Dev

--- src/android/Pim.java
+++ src/android/Pim.java
@@ -18,6 +18,21 @@ public class Pim extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
     	if (action.equals("open")) {
 			try {
+				int test = 0;
+				switch (test) {
+					case 1:
+						throw new IllegalAppVersionException();
+					break;
+					case 2:
+						throw new ComAlreadyInitialisedException();
+					break;
+					case 3:
+						throw new ComNotInitialisedException();
+					break;
+					case 4:
+						throw new IllegalIpAddressException();
+					break;
+				}
             	//myPayPoint.open("COM1", "v1.0.1");
             	callbackContext.success("Called open without errors");
             	return true;
