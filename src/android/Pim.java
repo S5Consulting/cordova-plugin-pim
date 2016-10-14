6655fe1f40f20a09865c794fc45b03c3226f88af
Svein Gunnar Larsen <Svein Gunnar Larsen>
2016-10-14 11:51:33 +0200

    Dev

--- src/android/Pim.java
+++ src/android/Pim.java
@@ -6,9 +6,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import no.point.paypoint.*;
 
-public class Pim extends CordovaPlugin {
+public class Pim extends CordovaPlugin implements PayPointListener {
 
 	PayPoint myPayPoint;
+	CallbackContext _callbackcontext;
 
 	public Pim() {
 		myPayPoint = new PayPoint();
@@ -16,6 +17,7 @@ public class Pim extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    	_callbackcontext = callbackContext;
     	if (action.equals("open")) {
 
 			try {
@@ -38,6 +40,44 @@ public class Pim extends CordovaPlugin {
 		}
 		return false;
     }
+
+    @Override
+	public void getPayPointEvent(PayPointEvent arg0) {
+  		PayPointResultEvent result;
+        PayPointStatusEvent status;
+
+        _callbackcontext.success("Whooho!");
+ 
+        switch(event.getEventType()){
+        	case PayPointEvent.STATUS_EVENT:
+            		// Treat status event
+                status = (PayPointStatusEvent)event;
+                if(status.getStatusType()==PayPointStatusEvent.STATUS_DISPLAY){
+                	// Treat display
+                }else if(status.getStatusType()==PayPointStatusEvent.STATUS_CARD_INFO){
+                	// Treat card info
+                }else if(status.getStatusType()==PayPointStatusEvent.STATUS_READY_FOR_TRANS){
+                    // Terminal ready for transaction
+                }
+            break;
+            case PayPointEvent.RESULT_EVENT:
+            	// Treat transaction result
+                result = (PayPointResultEvent)event;
+                // Check if result ok
+                if(result.getResult==PayPointResultEvent.RESULT_OK){
+                	if(result.getAccumulator==PayPointResultEvent.ACCU_APPROVED_ONLINE){
+                    	// Financial transaction approved online
+                    } else if(result.getAccumulator==PayPointResultEvent.ACCU_APPROVED_OFFLINE){
+                    	// Financial transaction approved offline
+                    } else {
+                    	// Administrative transaction ok
+                    }
+                } else {
+                    	// Declined/cancelled transaction
+                }
+            break;
+		}
+	}
 }
 
 
