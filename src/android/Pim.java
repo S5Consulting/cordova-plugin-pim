f9bc39fc76c619dfa6ccaf82d1ff0a21cb9ba2dc
Svein Gunnar Larsen <Svein Gunnar Larsen>
2016-10-24 13:37:54 +0200

    Dev

--- src/android/Pim.java
+++ src/android/Pim.java
@@ -10,8 +10,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
